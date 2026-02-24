a7a4a8fb7ce25eba4f058a11947fa674a4f12904
PRAKUL HIREMATH <PRAKULHIREMATH03@GMAIL.COM>
2026-02-24 15:38:13 +0530

    Add process noise covariance to Kalman Smoother

--- src/inference/kalman_smoother.py
+++ src/inference/kalman_smoother.py
@@ -2,12 +2,13 @@ import numpy as np
 
 
 class KalmanSmoother:
-    def __init__(self, A):
+    def __init__(self, A, Q):
         self.A = A
+        self.Q = Q
 
     def smooth(self, filtered_means, filtered_covs):
         """
-        Rauch-Tung-Striebel (RTS) Smoother
+        Rauch–Tung–Striebel (RTS) Smoother
         """
 
         T, dim = filtered_means.shape
@@ -17,22 +18,32 @@ class KalmanSmoother:
 
         for t in reversed(range(T - 1)):
 
-            P = filtered_covs[t]
-            P_next = filtered_covs[t + 1]
+            P_t = filtered_covs[t]
+
+            # Predicted covariance P_{t+1|t}
+            P_pred = (
+                self.A @ P_t @ self.A.T + self.Q
+            )
 
             # Smoother gain
-            G = P @ self.A.T @ np.linalg.inv(
-                self.A @ P @ self.A.T
+            G = P_t @ self.A.T @ np.linalg.inv(P_pred)
+
+            # Mean update
+            smoothed_means[t] = (
+                filtered_means[t]
+                + G @ (
+                    smoothed_means[t + 1]
+                    - self.A @ filtered_means[t]
+                )
             )
 
-            smoothed_means[t] += G @ (
-                smoothed_means[t + 1]
-                - self.A @ filtered_means[t]
+            # Covariance update
+            smoothed_covs[t] = (
+                P_t
+                + G @ (
+                    smoothed_covs[t + 1]
+                    - P_pred
+                ) @ G.T
             )
-
-            smoothed_covs[t] += G @ (
-                smoothed_covs[t + 1]
-                - P_next
-            ) @ G.T
 
         return smoothed_means, smoothed_covs
